014398623cd44c0793fc0f991aed953e601eda95
Trubienko <73603076+Elli1234567890@users.noreply.github.com>
2023-12-23 22:08:57 +0300

    Update app.svelte
    Частично добавлено меню. Практическая работа Svelte под вопросом.

--- app.svelte
+++ app.svelte
@@ -1,1 +1,156 @@
+<!DOCTYPE html>
+<html lang="en">
 
+<head>
+    <meta charset="UTF-8" />
+    <title>Проект</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="preconnect" href="https://fonts.gstatic.com/">
+    <link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.css" />
+    <link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick-theme.css" />
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;500;700&display=swap" rel="stylesheet">
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
+    <script src="https://code.jquery.com/jquery-1.11.0.min.js"></script>
+    <script src="https://code.jquery.com/jquery-migrate-1.2.1.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
+</head>
+
+<body style="background-color:#fff">
+
+    <style>
+        body {
+            font-family: 'Montserrat';
+            margin: 0;
+            width: 100%;
+            font-weight: 500;
+            font-size: 16px;
+            padding: 0;
+        }
+    </style>
+
+    <header class="navbar default-navbar" id="navbar" role="banner">
+
+        <style>
+            #navbar {
+                display: none;
+                position: fixed;
+                background: #000;
+                bottom: -2px;
+                top: auto;
+                z-index: 100;
+                left: 0;
+                right: 0;
+                padding: 0;
+            }
+
+            h2 {
+                color: #050c33;
+                font-size: 24px;
+                font-weight: bold;
+            }
+        </style>
+
+        <div class="container">
+
+            <style>
+                .container {
+                    margin-left: auto;
+                    margin-right: auto;
+                    padding: 0px 15px;
+                }
+            </style>
+
+            <div class="navbar-logo">
+                <style>
+                    .navbar-logo {
+                        bottom: 8px;
+                    }
+                </style>
+                <img src="/img/drupal-coder.svg" alt="Логотип">
+                <style>
+                    .navbar-logo img {
+                        width: 37%;
+                    }
+                </style>
+            </div>
+            <div class="navbar-collapse-wrapper">
+                <div class="region region-navigation-collapsible">
+                    <nav role="navigation" aria-labelledby="block-bootstrap-dc-main-menu-menu">
+                        <ul class="menu nav navbar-default">
+                            <li>
+                                <a href="" class="is-active"
+                                   style="border-bottom: 3px solid; border-color: #f14d34;">ПОДДЕРЖКА DRUPAL</a>
+                            </li>
+                            <li class="expanded dropdown">
+                                <a href="" class="dropdown-toggle" data-toggle="dropdown" title="">АДМИНИСТРИРОВАНИЕ</a>
+                                <div class="container-fluid dropdown-menu-wrapper" style="position: absolute;">
+                                    <ul class="menu dropdown-menu">
+                                        <li>
+                                            <a href="" title="">МИГРАЦИЯ</a>
+                                        </li>
+                                        <li>
+                                            <a href="" title="">БЭКАПЫ</a>
+                                        </li>
+                                        <li>
+                                            <a href="" title="">АУДИТ БЕЗОПАСНОСТИ</a>
+                                        </li>
+                                        <li>
+                                            <a href="" title="">ОПТИМИЗАЦИЯ СКОРОСТИ</a>
+                                        </li>
+                                        <li>
+                                            <a href="" title="">ПЕРЕЕЗД НА HTTPS</a>
+                                        </li>
+                                    </ul>
+                                </div>
+                            </li>
+
+                            <li>
+                                <a href="" class="is-active">ПРОДВИЖЕНИЕ</a>
+                            </li>
+
+                            <li>
+                                <a href="" class="is-active">РЕКЛАМА</a>
+                            </li>
+                            <li class="expanded dropdown">
+                                <a href="" class="dropdown-toggle" data-toggle="dropdown" title="">О НАС</a>
+                                <div class="container-fluid dropdown-menu-wrapper" style="position: absolute;">
+                                    <ul class="menu dropdown-menu">
+                                        <li>
+                                            <a href="" title="">КОМАНДА</a>
+                                        </li>
+                                        <li>
+                                            <a href="" title="">DRUPALGIVE</a>
+                                        </li>
+                                        <li>
+                                            <a href="" title="">БЛОГ</a>
+                                        </li>
+                                        <li>
+                                            <a href="" title="">КУРСЫ DRUPAL</a>
+                                        </li>
+                                    </ul>
+                                </div>
+                            </li>
+                            <li>
+
+                            <a href="" class="is-active">ПРОЕКТы</a>
+                            </li>
+
+                            <li>
+                                <a href="" class="is-active">КОНТТАКТЫ</a>
+                            </li>
+                        </ul>
+                    </nav>
+                </div>
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+            </div>
+        </div>
+    </header>
+</html>
