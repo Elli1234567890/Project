e1725c3ecd988f6091ac18cbb3dc0a76a08b98d9
Trubienko <73603076+Elli1234567890@users.noreply.github.com>
2023-12-24 11:55:22 +0300

    Update app.svelte

--- app.svelte
+++ app.svelte
@@ -1,1 +1,99 @@
+<main>
+    <ol>      
+      <h2><center>Нумерованный список ссылок</center></h2>
+      <li><a href="http://kubsu.ru">КубГУ</a></li>      
+      <li><a href="https://kubsu.ru/">КубГУ</a></li>      
+      <li><a href="http://kubsu.ru/"><img src="https://raw.githubusercontent.com/Elli1234567890/Elli1234567890.github.io/main/image.jpg" width="400" height="300" alt="Цветок"/></a></li>     
+      <li><a href="#form">Фрагмент текущей страницы</a></li>
+      <li><a href="/ooo.html">Сокращённая ссылка на внутреннюю страницу</a></li>  
+      <li><a href="/#">Сокращённая ссылка на главную страницу</a></li>      
+      <li><a href="#node">Ссылка на фрагмент текущей страницы</a></li>
+      <li><a href="https://github.com/Elli1234567890/Elli1234567890.github.io/?parametr1=issues&parametr2=pulls&parametr3=actions">Ссылка с тремя параметрами в URL</a></li> 
+      <li>
+        <th id="s0177164"></th>
+        <a href="http://kubsu.ru/#s0177164" class="site" target="blank">Ссылка с параметром id в URL</a>
+      </li>  
+      <li><a href="yyy.html">Относительная ссылка на страницу в текущем каталоге</a></li>
+      <li><a href="../yyy.html">Относительная ссылка на страницу в каталоге уровнем выше текущего</a></li>
+      <li><a href="../../yyy.html">Относительная ссылка на страницу в каталоге двумя уровнями выше текущего</a></li>
+      <li><p>Контекстной считается <a href="https://info-master.su/programming/web/html/link-attributes.php#tabindex">ссылка</a>, размещенная в фрагменте текстового контента, в который она вписывается по смыслу.</p></li>
+      <li><a href=" https://ru.wikipedia.org/wiki/Python#Возможности">Ссылка на фрагмент страницы стороннего сайта</a></li><br>
+      
+      <li>
+        <img src="https://i.pinimg.com/originals/8b/6c/b4/8b6cb430f1bd571e209c8a8d08e5a194.jpg" width="200" height="200" alt="Кубик" usemap="#mymap">
+          <map name="mymap">
+            <area shape="rect" coords="40,25,100,150" href="https://codeforces.net/problemset/problem/1872/G" alt="Круглая область">
+            <area shape="circle" coords="100,10,25" href="http://kubsu.ru/" alt="Прямоугольная область">
+          </map>
+      </li>
+      
+      <li><a href>Ссылка с пустым href</a></li>
+      <li><a>Ссылка без href</a></li>
+      <li><a href="http://kubsu.ru/" rel="notfollow">Ссылка на КубГУ, по которой запрещен переход поисковикам</a></li>
+      <li><a href="http://kubsu.ru/" rel="notfollow">Ссылка на КубГУ, запрещенная для индексации поисковиками</a></li>
+      <li>Всё задание с ссылками и есть нумерованный список ссылок с подписями title</li>  
+      <li><a href="ftp://user:7851@example.com/myfile.zip">Несуществующий файл на сервере FTP</a></li>
+    </ol>
+    
+    <li><a href="https://github.com/Elli1234567890/Elli1234567890.github.io/blob/main/index.html">Ссылка на githab</a></li>
 
+    
+    <h2>Форма</h2>
+      <form action="https://www.example.com" method="POST"  id="form">          
+        <label>
+          ФИО:<br>
+          <input name="field-text" placeholder="Трубиенко Татьяна Алексеевна" type="name">
+        </label><br>
+        <label>
+          Телефон:<br>
+          <input name="field-tel" placeholder="+7-123-45-67" type="email">
+        </label><br>
+        <label>
+          Email:<br>
+          <input name="field-email" placeholder="rinatiadzua@gmail.com" type="email">
+        </label><br>
+        <label>
+          Дата рождения:<br>
+          <input name="field-date" value="2000-01-01" type="date">
+          <br>
+          Пол:<br>
+          <label>
+            <input type="radio" name="radio-1" value="М">
+            М
+          </label>
+          <label>
+            <input type="radio" name="radio-2" value="Ж">
+            Ж
+          </label>
+               
+        </label><br>
+        <label>
+          Любимый язык программирования:
+          <br>
+          <select name="field-name[]" multiple="multiple" size="4">
+            <option value="Значение 1">Paskal</option>
+            <option value="Значение 2">C</option>
+            <option value="Значение 3">C++</option>
+            <option value="Значение 4">JavaScript</option>
+            <option value="Значение 5">PHP</option>
+            <option value="Значение 6">Python</option>
+            <option value="Значение 7">Java</option>
+            <option value="Значение 8">Haskel</option>
+            <option value="Значение 9">Clojure</option>
+            <option value="Значение 10">Prolog</option>
+            <option value="Значение 11">Scala</option>
+          </select>
+        </label><br>
+        <label>
+          Биография:<br>
+       <textarea> </textarea>
+        </label><br>
+        С контрактом ознакомлен(а):<br>
+        <label>
+          <input type="checkbox" checked="checked" name="check-1">
+        </label><br>
+          <input type="submit" value="Сохранить">
+        </form>
+    <footer class="footer" id="footer">
+    </footer>
+</main>
